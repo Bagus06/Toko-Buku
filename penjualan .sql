a24f330378fdf358d7a525faa63d55e1e812e1f9
Bagus06 <Bagusfikrianto7@gmail.com>
2019-11-12 11:40:24 +0700

    db up

--- penjualan .sql
+++ penjualan .sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 25 Okt 2019 pada 09.47
+-- Generation Time: 12 Nov 2019 pada 11.37
 -- Versi Server: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.19-0ubuntu0.18.04.2
+-- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -45,7 +45,8 @@ CREATE TABLE `tb_buku` (
 --
 
 INSERT INTO `tb_buku` (`id_buku`, `judul`, `noisbn`, `penulis`, `penerbit`, `tahun`, `stok`, `harga_pokok`, `harga_jual`, `ppn`, `diskon`) VALUES
-(6, 'Belajar PHP', '211', 'Bagus', 'Sinar Jaya', 2019, 12, 19900, 20000, 2000, 2100);
+(6, 'Belajar PHP', '211', 'Koresy', 'Soso Hoha Jaya', 2019, 46, 30000, 35000, 2000, 1000),
+(7, 'Android Studio', '1121', 'Sandi', 'erlangga', 2019, 100, 100000, 120000, 2000, 1000);
 
 -- --------------------------------------------------------
 
@@ -71,6 +72,28 @@ INSERT INTO `tb_distributor` (`id_distributor`, `nama_distributor`, `alamat`, `t
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_jual`
+--
+
+CREATE TABLE `tb_jual` (
+  `id_jual` varchar(10) NOT NULL,
+  `total` varchar(50) NOT NULL,
+  `uang` varchar(50) NOT NULL,
+  `kembali` varchar(50) NOT NULL,
+  `id_kasir` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_jual`
+--
+
+INSERT INTO `tb_jual` (`id_jual`, `total`, `uang`, `kembali`, `id_kasir`, `tanggal`) VALUES
+('PJN00001', '60800', '70000', '9200', 5, '2019-11-06');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_kasir`
 --
 
@@ -90,8 +113,29 @@ CREATE TABLE `tb_kasir` (
 --
 
 INSERT INTO `tb_kasir` (`id_kasir`, `nama`, `alamat`, `telepon`, `status`, `username`, `password`, `akses`) VALUES
-(2, 'Ilham Agustian', 'desa dermolo', '081325529851', 'aktif', 'admin', 'c81e728d9d4c2f636f067f89cc14862c', 'admin'),
-(5, 'Khoirul Annas', 'Dermolo', '08', 'aktif', 'annas', 'c4ca4238a0b923820dcc509a6f75849b', 'kasir');
+(2, 'Koresy Setian Ks', 'Bondo', '089124563457', 'aktif', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(5, 'Khoirul Annas', 'Srobyong', '085338578123', 'aktif', 'kasir', 'c7911af3adbd12a035b289556d96470a', 'kasir');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_keranjang` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_kasir` int(11) NOT NULL,
+  `jumlah` varchar(40) NOT NULL,
+  `jumlah_harga` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_keranjang`
+--
+
+INSERT INTO `tb_keranjang` (`id_keranjang`, `id_buku`, `id_kasir`, `jumlah`, `jumlah_harga`) VALUES
+(1, 6, 5, '4', '79600');
 
 -- --------------------------------------------------------
 
@@ -112,8 +156,8 @@ CREATE TABLE `tb_pasok` (
 --
 
 INSERT INTO `tb_pasok` (`id_pasok`, `id_distributor`, `id_buku`, `jumlah`, `tanggal`) VALUES
-(3, 3, 6, 2, '22-10-2019'),
-(4, 3, 6, 10, '22-10-2019');
+(8, 3, 6, 24, '06-11-2019'),
+(9, 3, 7, 38, '06-11-2019');
 
 -- --------------------------------------------------------
 
@@ -124,11 +168,18 @@ INSERT INTO `tb_pasok` (`id_pasok`, `id_distributor`, `id_buku`, `jumlah`, `tang
 CREATE TABLE `tb_penjualan` (
   `id_penjualan` int(11) NOT NULL,
   `id_buku` int(11) NOT NULL,
-  `id_kasir` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `total` int(11) NOT NULL,
-  `tanggal` date NOT NULL
+  `jumlah_harga` int(11) NOT NULL,
+  `id_jual` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_penjualan`
+--
+
+INSERT INTO `tb_penjualan` (`id_penjualan`, `id_buku`, `jumlah`, `jumlah_harga`, `id_jual`) VALUES
+(32, 6, 2, 39800, 'PJN00001'),
+(33, 7, 1, 21000, 'PJN00001');
 
 --
 -- Indexes for dumped tables
@@ -147,10 +198,25 @@ ALTER TABLE `tb_distributor`
   ADD PRIMARY KEY (`id_distributor`);
 
 --
+-- Indexes for table `tb_jual`
+--
+ALTER TABLE `tb_jual`
+  ADD PRIMARY KEY (`id_jual`),
+  ADD KEY `id_kasir` (`id_kasir`);
+
+--
 -- Indexes for table `tb_kasir`
 --
 ALTER TABLE `tb_kasir`
   ADD PRIMARY KEY (`id_kasir`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_keranjang`),
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `id_kasir` (`id_kasir`);
 
 --
 -- Indexes for table `tb_pasok`
@@ -165,8 +231,8 @@ ALTER TABLE `tb_pasok`
 --
 ALTER TABLE `tb_penjualan`
   ADD PRIMARY KEY (`id_penjualan`),
-  ADD KEY `id_kasir` (`id_kasir`),
-  ADD KEY `id_buku` (`id_buku`);
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `id_jual` (`id_jual`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -176,7 +242,7 @@ ALTER TABLE `tb_penjualan`
 -- AUTO_INCREMENT for table `tb_buku`
 --
 ALTER TABLE `tb_buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tb_distributor`
 --
@@ -188,15 +254,20 @@ ALTER TABLE `tb_distributor`
 ALTER TABLE `tb_kasir`
   MODIFY `id_kasir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `tb_pasok`
 --
 ALTER TABLE `tb_pasok`
-  MODIFY `id_pasok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pasok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `tb_penjualan`
 --
 ALTER TABLE `tb_penjualan`
-  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
@@ -212,8 +283,7 @@ ALTER TABLE `tb_pasok`
 -- Ketidakleluasaan untuk tabel `tb_penjualan`
 --
 ALTER TABLE `tb_penjualan`
-  ADD CONSTRAINT `tb_penjualan_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`),
-  ADD CONSTRAINT `tb_penjualan_ibfk_2` FOREIGN KEY (`id_kasir`) REFERENCES `tb_kasir` (`id_kasir`);
+  ADD CONSTRAINT `tb_penjualan_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
